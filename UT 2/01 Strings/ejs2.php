e1b643e70ef52912a6b9492298be0348ca5653ca
unknown <javierre13@hotmail.com>
2024-09-19 13:46:40 +0200

    UT2-01-ejs2

--- UT 2/01 Strings/ejs2.php
+++ UT 2/01 Strings/ejs2.php
@@ -1,24 +1,23 @@
 <HTML>
-  <HEAD>
-    <TITLE> EJ1-Conversion IP Decimal a Binario </TITLE>
-  </HEAD>
-  <BODY>
-    <?php
-      $ip="192.18.16.204";
-	  
-	  echo "IP " . $ip . " en binario es  ";
-	  
-      $ipSeparada = explode(".", $ip);
-	  for ($i = 0; $i < 4; $i++) {
-		$ipSeparadaBin = decbin($ipSeparada[$i]);
-		while (strlen($ipSeparadaBin) < 8) {
-			$ipSeparadaBin = "0" . $ipSeparadaBin;
-		}
-		if ($i < 3)
-			echo $ipSeparadaBin . ".";
-		else
-			echo $ipSeparadaBin;
-	  }
-	?>
-  </BODY>
+	<HEAD>
+		<TITLE> EJ1-Conversion IP Decimal a Binario </TITLE>
+	</HEAD>
+	<BODY>
+		<?php
+			$ip="192.18.16.204";
+			
+			echo "IP " . $ip . " en binario es  ";
+			$ipSeparada = explode(".", $ip);
+			for ($i = 0; $i < 4; $i++) {
+				$ipSeparadaBin = decbin($ipSeparada[$i]);
+				while (strlen($ipSeparadaBin) < 8) {
+					$ipSeparadaBin = "0" . $ipSeparadaBin;
+				}
+				if ($i < 3)
+					echo $ipSeparadaBin . ".";
+				else
+					echo $ipSeparadaBin;
+			}
+		?>
+	</BODY>
 </HTML>
