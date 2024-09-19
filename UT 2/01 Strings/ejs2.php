ebb9cf014b4546a2c00f6a425eac4ca62e2c36c0
unknown <javierre13@hotmail.com>
2024-09-19 13:41:53 +0200

    UT2-01-ejs2

---
+++ UT 2/01 Strings/ejs2.php
@@ -0,0 +1,21 @@
+<HTML>
+  <HEAD>
+    <TITLE> EJ1-Conversion IP Decimal a Binario </TITLE>
+  </HEAD>
+  <BODY>
+    <?php
+      $ip="192.18.16.204";
+      $ipSeparada = explode(".", $ip);
+	  for ($i = 0; $i < 4; $i++) {
+		$ipSeparadaBin = decbin($ipSeparada[$i]);
+		while (strlen($ipSeparadaBin) < 8) {
+			$ipSeparadaBin = "0" . $ipSeparadaBin;
+		}
+		if ($i < 3)
+			echo $ipSeparadaBin . ".";
+		else
+			echo $ipSeparadaBin;
+	  }
+	?>
+  </BODY>
+</HTML>
