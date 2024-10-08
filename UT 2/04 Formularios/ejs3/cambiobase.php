28accca5e9b0497b35f67272787e5b10abf9f6a2
unknown <javierre13@hotmail.com>
2024-10-08 10:15:30 +0200

    UT2-04-ejs3

--- UT 2/04 Formularios/ejs3/cambiobase.php
+++ UT 2/04 Formularios/ejs3/cambiobase.php
@@ -7,8 +7,10 @@
 </head>
   <body>
     <h1>Conversor numérico</h1>
-    <?php  
-     require 'fcambiobase.php';
+    <?php
+      $decimal = $_REQUEST['decimal'];
+      $conversion = $_REQUEST['conversion'];
+      $tabla = cambiarBase($decimal, $conversion);
 
       echo "
         <form>
@@ -20,6 +22,38 @@
           </table>
         </form>
       ";
+
+      function cambiarBase($decimal, $conversion) {
+        switch ($conversion) {
+          case 'binario':
+            $binario = decbin($decimal);
+            $tabla = "<tr><td>Binario</td><td>$binario</td></tr>";
+            break;
+
+          case 'octal':
+            $octal = decoct($decimal);
+            $tabla = "<tr><td>Octal</td><td>$octal</td></tr>";
+            break;
+          
+          case 'hexadecimal':
+            $hexadecimal = strtoupper(dechex($decimal));
+            $tabla = "<tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>";
+            break;
+
+          case 'todos':
+            $binario = decbin($decimal);
+            $octal = decoct($decimal);
+            $hexadecimal = strtoupper(dechex($decimal));
+            $tabla = "
+                      <tr><td>Binario</td><td>$binario</td></tr>
+                      <tr><td>Octal</td><td>$octal</td></tr>
+                      <tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>
+                      ";
+            break;
+        }
+
+        return $tabla;
+      }
     ?>
   </body>
 </html>
--- UT 2/04 Formularios/ejs3/fcambiobase.php
+++ UT 2/04 Formularios/ejs3/fcambiobase.php
@@ -1,32 +1,84 @@
-<?php
-  $decimal = $_REQUEST['decimal'];
-  $conversion = $_REQUEST['conversion'];
-  
-  switch ($conversion) {
-    case 'binario':
-      $binario = decbin($decimal);
-      $tabla = "<tr><td>Binario</td><td>$binario</td></tr>";
-      break;
+<!DOCTYPE html>
+<html lang='es'>
+  <head>
+    <title>EJ3-Conversor numérico</title>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+  <body>
+    <h1>Conversor numérico</h1>
+    <form action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>' method='POST'>
+      <label for='decimal'>Número decimal: </label>
+      <input type='text' name='decimal' id='decimal'>
+      <br><br>
+      <label for='conversion'>Convertir a:</label>
+      <br>
+      <input type='radio' name='conversion' value='binario' id='binario'>
+      <label for='binario'>Binario</label>
+      <br>
+      <input type='radio' name='conversion' value='octal' id='octal'>
+      <label for='octal'>Octal</label>
+      <br>
+      <input type='radio' name='conversion' value='hexadecimal' id='hexadecimal'>
+      <label for='hexadecimal'>Hexadecimal</label>
+      <br>
+      <input type='radio' name='conversion' value='todos' id='todos'>
+      <label for='todos'>Todos sistemas</label>
+      <br><br>
+      <button type='submit'>Enviar</button>
+      <button type='reset'>Borrar</button>
+      <br><br>
+    </form>
 
-    case 'octal':
-      $octal = decoct($decimal);
-      $tabla = "<tr><td>Octal</td><td>$octal</td></tr>";
-      break;
+    <?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $decimal = $_REQUEST['decimal'];
+        $conversion = $_REQUEST['conversion'];
+        $tabla = cambiarBase($decimal, $conversion);
 
-    case 'hexadecimal':
-      $hexadecimal = strtoupper(dechex($decimal));
-      $tabla = "<tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>";
-      break;
-      
-    case 'todos':
-      $binario = decbin($decimal);
-      $octal = decoct($decimal);
-      $hexadecimal = strtoupper(dechex($decimal));
-      $tabla = "
-                <tr><td>Binario</td><td>$binario</td></tr>
-                <tr><td>Octal</td><td>$octal</td></tr>
-                <tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>
-                ";
-      break;
-  }
-?>
+        echo "
+          <form>
+            <label for='decimal'>Número decimal: </label>
+            <input type='text' name='decimal' id='decimal' value='$decimal' readonly>
+            <br><br>
+            <table border='1'>
+              $tabla
+            </table>
+          </form>
+        ";
+      }
+
+      function cambiarBase($decimal, $conversion) {
+        switch ($conversion) {
+          case 'binario':
+            $binario = decbin($decimal);
+            $tabla = "<tr><td>Binario</td><td>$binario</td></tr>";
+            break;
+
+          case 'octal':
+            $octal = decoct($decimal);
+            $tabla = "<tr><td>Octal</td><td>$octal</td></tr>";
+            break;
+          
+          case 'hexadecimal':
+            $hexadecimal = strtoupper(dechex($decimal));
+            $tabla = "<tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>";
+            break;
+
+          case 'todos':
+            $binario = decbin($decimal);
+            $octal = decoct($decimal);
+            $hexadecimal = strtoupper(dechex($decimal));
+            $tabla = "
+                      <tr><td>Binario</td><td>$binario</td></tr>
+                      <tr><td>Octal</td><td>$octal</td></tr>
+                      <tr><td>Hexadecimal</td><td>$hexadecimal</td></tr>
+                      ";
+            break;
+        }
+
+        return $tabla;
+      }
+    ?>
+  </body>
+</html>
