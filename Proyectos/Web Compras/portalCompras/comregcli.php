f642a8ca38bfefbb9b3b18412ed3637b2c1f4238
unknown <javierre13@hotmail.com>
2024-12-04 10:49:01 +0100

    registro de clientes con contraseña

---
+++ Proyectos/Web Compras/portalCompras/comregcli.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Alta almacenes - Web Compras</title>
+    <link rel='stylesheet' href='../css/comun.css'>
+  </head>
+  <body>
+    <?php
+      include '../handlerErrores.php';
+      include '../conexionBd.php';
+      include 'fComregcli.php';
+    ?>
+
+    <nav>
+      <a href='../gestionInternaClientes.html'>Atrás</a>
+      <a href='comregcli.php'>Registro de clientes</a>
+      <a href='comlogincli.php'>Login</a>
+      <a href='comprocli.php'>Compra de productos</a>
+      <a href='comconscli.php'>Consulta de compras</a>
+    </nav>
+
+    <div>
+      <b>Crear nuevo almacen</b>
+      <br><br>
+    </div>
+
+    <form action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>' method='POST'>
+      <b>NIF: </b><input type='text' name='nif'>
+      <br><br>
+      <b>Nombre: </b><input type='text' name='nombre'>
+      <br><br>
+      <b>Apellido: </b><input type='text' name='apellido'>
+      <br><br>
+      <b>CP: </b><input type='text' name='cp'>
+      <br><br>
+      <b>Dirección: </b><input type='text' name='direccion'>
+      <br><br>
+      <b>Ciudad: </b><input type='text' name='ciudad'>
+      <br><br>
+      <input type='submit' value='Registrar cliente'>
+    </form>
+    
+    <?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nif = $_POST['nif'];
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $cp = $_POST['cp'];
+        $direccion = $_POST['direccion'];
+        $ciudad = $_POST['ciudad'];
+        
+        limpiar($nif);
+        limpiar($nombre);
+        limpiar($apellido);
+        limpiar($cp);
+        limpiar($direccion);
+        limpiar($ciudad);
+        
+        $usuario = generarUsuario($nombre);
+
+        validarNif($nif);
+        buscarNifRepetido($nif);
+        validarUsuario($usuario);
+        buscarUsuarioRepetido($usuario);
+
+        $clave = generarClave($apellido);
+
+        registrarCliente($nif, $nombre, $apellido, $cp, $direccion, $ciudad, $usuario, $clave);
+      }
+    ?>
+  </body>
+</html>
---
+++ Proyectos/Web Compras/portalCompras/fComregcli.php
@@ -0,0 +1,74 @@
+<?php
+  function limpiar(&$value) {
+    $value = trim($value);
+    $value = stripslashes($value);
+    $value = htmlspecialchars($value);
+  }
+
+  function generarUsuario($nombre) {
+    return str_replace(' ', '', strtolower($nombre));
+  }
+
+  function validarNif($nif) {
+    $regex = '/^[0-9]{8}[A-Za-z]$/';
+
+    if (!preg_match($regex, $nif))
+      trigger_error('Debes introducir un DNI válido', E_USER_WARNING);
+  }
+
+  function buscarNifRepetido($nif) {
+    $resultado = buscarNif($nif);
+
+    if (!empty($resultado))
+      trigger_error('Ya existe un cliente con ese NIF', E_USER_WARNING);
+  }
+
+  function buscarNif($nif) {
+    $sql = "SELECT nif
+              FROM cliente
+              WHERE nif = :nif";
+    $params = [':nif' => $nif];
+
+    return operarBd($sql, $params);
+  }
+
+  function validarUsuario($usuario) {
+    if ($usuario == '')
+      trigger_error('El nombre no puede estar vacio', E_USER_WARNING);
+  }
+
+  function buscarUsuarioRepetido($usuario) {
+    $resultado = buscarUsuario($usuario);
+
+    if (!empty($resultado))
+      trigger_error('Ya existe un cliente con ese nombre', E_USER_WARNING);
+  }
+
+  function buscarUsuario($usuario) {
+    $sql = "SELECT usuario
+              FROM cliente
+              WHERE usuario = :usuario";
+    $params = [':usuario' => $usuario];
+
+    return operarBd($sql, $params);
+  }
+
+  function generarClave($apellido) {
+    return password_hash(strrev(str_replace(' ', '', strtolower($apellido))), PASSWORD_DEFAULT);
+  }
+
+  function registrarCliente($nif, $nombre, $apellido, $cp, $direccion, $ciudad, $usuario, $clave) {
+    $valido = registrar($nif, $nombre, $apellido, $cp, $direccion, $ciudad, $usuario, $clave);
+
+    if ($valido)
+      echo "Se registró el cliente con NIF $nif correctamente";
+  }
+
+  function registrar($nif, $nombre, $apellido, $cp, $direccion, $ciudad, $usuario, $clave) {
+    $sql = "INSERT INTO cliente(nif, nombre, apellido, cp, direccion, ciudad, usuario, clave)
+              VALUES(:nif, :nombre, :apellido, :cp, :direccion, :ciudad, :usuario, :clave)";
+    $params = [':nif' => $nif, ':nombre' => $nombre, ':apellido' => $apellido, ':cp' => $cp, ':direccion' => $direccion, ':ciudad' => $ciudad, ':usuario' => $usuario, ':clave' => $clave];
+
+    return operarBd($sql, $params);
+  }
+?>
