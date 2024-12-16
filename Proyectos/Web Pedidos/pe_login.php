fbe7cad7f4a5daed701e8ada56f745338b362381
unknown <javierre13@hotmail.com>
2024-12-16 09:29:55 +0100

    Login

--- Proyectos/Web Pedidos/pe_login.php
+++ Proyectos/Web Pedidos/pe_login.php
@@ -9,11 +9,6 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Login - Web Pedidos</title>
-    <style>
-      body {
-        background-color: #274923;
-      }
-    </style>
   </head>
   <body>
     <?php
