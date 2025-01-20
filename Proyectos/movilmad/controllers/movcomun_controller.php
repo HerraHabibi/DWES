2251aa435eb68295044b08a8f2ff56ec4ea77998
unknown <javierre13@hotmail.com>
2025-01-20 11:05:47 +0100

    Login fix

--- Proyectos/movilmad/controllers/movcomun_controller.php
+++ Proyectos/movilmad/controllers/movcomun_controller.php
@@ -2,8 +2,8 @@
   // Se inicia la sesión
   session_start();
 
+  require_once('controllers/movcomun_controller_funciones.php');
+
   // Se establece el Error handler
   set_error_handler('errores', E_USER_WARNING);
-
-  require_once('controllers/movcomun_controller_funciones.php');
 ?>
